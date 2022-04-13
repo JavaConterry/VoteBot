e274a8d04926f7f1b64e0e8d8b70bbb47d4a6330
JavaConterry <conterry05@gmail.com>
2022-04-13 12:15:59 +0300

    solving git problems

--- bot.js
+++ bot.js
@@ -1,28 +1,10 @@
 "use strict";
 exports.__esModule = true;
 var Telegraf = require('telegraf').Telegraf;
+var UserService_1 = require("./UserService");
 var fs = require("fs");
 var BOT_TOKEN = fs.readFileSync('token.txt', 'utf8');
 var bot = new Telegraf(BOT_TOKEN);
-function createUser(config) {
-    var newUser = { username: "common username", vote_status: false };
-    newUser.username = config.username;
-    return newUser;
-}
-// let myUser = createUser({username:"Fill"})
-// pushUserIntoJSON(myUser)
-function pushUserIntoJSON(userToPush) {
-    var data = JSON.parse(fs.readFileSync('datasource.json', 'utf8'));
-    var usersDataLength = data.user.length;
-    for (var i = 0; i < usersDataLength; i++) {
-        if (data.user[i].username == userToPush.username) {
-            return "username already exists";
-        }
-    }
-    data.user[usersDataLength] = userToPush;
-    fs.writeFileSync('datasource.json', JSON.stringify(data, null, 2));
-    return "registration seccess";
-}
 //logic {{{{{{{{
 bot.start(function (ctx) {
     ctx.reply('start phrase');
@@ -31,7 +13,7 @@ bot.command('get_random', function (ctx) {
     ctx.reply(Math.floor(Math.random() * 100));
 });
 bot.command('register', function (ctx) {
-    var log_result = pushUserIntoJSON({ username: ctx.update.message.from.username });
+    var log_result = (0, UserService_1.pushUserIntoJSON)({ username: ctx.update.message.from.username });
     ctx.reply(log_result);
 });
 bot.on('text', function (ctx) {
--- bot.ts
+++ bot.ts
@@ -1,28 +1,9 @@
 const { Telegraf } = require('telegraf')
-import { IUser } from './IUser'
+import { pushUserIntoJSON } from './UserService'
 import * as fs from 'fs';
 
 const BOT_TOKEN = fs.readFileSync('token.txt','utf8');
 const bot = new Telegraf(BOT_TOKEN)
-
-function createUser(config: IUser):{username:string; vote_status:boolean} {
-    let newUser = {username:"common username", vote_status:false};
-    newUser.username = config.username;
-    return newUser;
-}
-
-function pushUserIntoJSON(userToPush:IUser){
-    let data = JSON.parse(fs.readFileSync('datasource.json','utf8'));
-    const usersDataLength = data.user.length
-    for (let i = 0; i < usersDataLength; i++) {
-        if(data.user[i].username==userToPush.username){
-            return "username already exists"
-        }
-    }
-    data.user[usersDataLength] = userToPush
-    fs.writeFileSync('datasource.json', JSON.stringify(data, null, 2));
-    return "registration seccess"
-}
 
 //logic {{{{{{{{
 bot.start(ctx => {
